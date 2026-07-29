e10b204e6df0977e5fc89cedbc2db8070a1c2b10
sbjoni574-boop <sbjoni574@gmail.com>
2026-07-29 16:25:16 +0545

    Add files via upload

---
+++ viewer.js
@@ -0,0 +1,6 @@
+const connectBtn = document.getElementById("connectBtn");
+const remoteVideo = document.getElementById("remoteVideo");
+
+connectBtn.addEventListener("click", () => {
+    alert("Viewer page ready.\nServer aur WebRTC signaling abhi add karni baaki hai.");
+});

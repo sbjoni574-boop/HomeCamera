1824ef5251639920f9bddb0ab6eb291f3ecad170
sbjoni574-boop <sbjoni574@gmail.com>
2026-07-29 19:14:03 +0545

    Update server.js

--- server.js
+++ server.js
@@ -17,41 +17,14 @@ io.on("connection", (socket) => {
 
     console.log("User Connected :", socket.id);
 
-    socket.on("signal", (data) => {
-        socket.broadcast.emit("signal", data);
+    socket.on("camera-ready", () => {
+        socket.broadcast.emit("camera-ready");
     });
 
-    socket.on("disconnect", () => {
-        console.log("Disconnected :", socket.id);
+    socket.on("viewer-ready", () => {
+        socket.broadcast.emit("viewer-ready");
     });
 
-});
-
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-});
-
-app.use(express.static(__dirname));
-
-io.on("connection", (socket) => {
-
-    console.log("User Connected :", socket.id);
-socket.on("camera-ready", () => {
-    socket.broadcast.emit("camera-ready");
-});
-
-socket.on("viewer-ready", () => {
-    socket.broadcast.emit("viewer-ready");
-});
     socket.on("signal", (data) => {
         socket.broadcast.emit("signal", data);
     });

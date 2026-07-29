9d3ca06bf1887aa04dcc6e8cc688aa4c0670b94b
sbjoni574-boop <sbjoni574@gmail.com>
2026-07-29 17:09:44 +0545

    Refactor server to use Express and Socket.io

--- server.js
+++ server.js
@@ -27,6 +27,37 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
-    console.log("Server Running On Port 3000");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+app.use(express.static(__dirname));
+
+io.on("connection", (socket) => {
+
+    console.log("User Connected :", socket.id);
+
+    socket.on("signal", (data) => {
+        socket.broadcast.emit("signal", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected :", socket.id);
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server Running :", PORT);
 });
